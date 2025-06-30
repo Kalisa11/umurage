4678b4fc45a275cb5d652bdf11d13cdeaed47a65
honorekalisa <h.kalisa@alustudent.com>
2025-07-01 00:54:26 +0200

    Refactor user relations by removing upcomingActivities and cleaning up imports

--- server/src/db/relations.ts
+++ server/src/db/relations.ts
@@ -1,27 +1,10 @@
 import { relations } from "drizzle-orm/relations";
-import {
-  users,
-  upcomingActivities,
-  submissions,
-  artists,
-  categories,
-} from "./schema";
+import { users, submissions, artists, categories } from "./schema";
 
 export const userRelations = relations(users, ({ many }) => ({
-  upcomingActivities: many(upcomingActivities),
   submissions: many(submissions),
   reviewedSubmissions: many(submissions, { relationName: "reviewer" }),
 }));
-
-export const upcomingActivityRelations = relations(
-  upcomingActivities,
-  ({ one }) => ({
-    user: one(users, {
-      fields: [upcomingActivities.createdBy],
-      references: [users.id],
-    }),
-  })
-);
 
 export const submissionRelations = relations(submissions, ({ one }) => ({
   user: one(users, {

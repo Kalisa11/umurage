7accb7ad52d941a9274c122202432caa089cc66e
honorekalisa <h.kalisa@alustudent.com>
2025-07-01 01:11:45 +0200

    Update production CORS origin to reflect new client deployment URL

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://umurage-delta.vercel.app"
+        ? "https://umurage-client.vercel.app"
         : [
             "http://localhost:3000",
             "http://localhost:3001",
